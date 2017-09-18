f6c2cb2d960ca68f03348673dcd4bc499593f67f
Franklin García <franklin.garcia@pawadominicana.com>
2017-09-18 17:30:51 -0400

    Project skel added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to this example</h2>
+        </div>
+        <div className="App-container">
+        <p className="App-intro">
+          
+        </p>
+        </div>
+        <div className="App-footer">
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
